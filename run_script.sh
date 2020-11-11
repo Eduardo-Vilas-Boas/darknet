391a33d0843f66bdf1970bd860cbcc77d5e7d061
Eduardo Guerra <eduardo.vilasboas.guerra@gmail.com>
2020-11-11 08:25:15 +0000

    Predictions images splitted by category in folder predictions

--- run_script.sh
+++ run_script.sh
@@ -1,12 +1,29 @@
 #/bin/bash
 
-for file in ./obj/*.jpg
+mkdir ./predicitons
+mkdir ./predicitons/Fire
+mkdir ./predicitons/Smoke
+
+for file in /content/darknet/data/obj/*.jpg
+
 do
 	/content/darknet/darknet detector test data/obj.data cfg/yolov4.cfg ../yolov4_5000.weights ${file} -iou_thresh 0.3 -thresh 0.05
 
-	mkdir "${file%.jpg}"
+	var="${file%.jpg}"
+  	new_prefix="${var##*/}"
 
-	cp ./*Fire.jpg /"${file%.jpg}"
-	cp ./*Smoke.jpg /"${file%.jpg}"
+	for file_prediction_fire in ./*Fire.jpg
+	do		
+		#echo "${file_prediction_fire}"
+		no_prefix_name="${file_prediction_fire##*/}"
+		mv ${file_prediction_fire} ./predicitons/Fire/${new_prefix}_${no_prefix_name}
+	done
+
+	for file_prediction_smoke in ./*Smoke.jpg
+	do
+		#echo "${file_prediction_smoke}"
+		no_prefix_name="${file_prediction_smoke##*/}"
+		mv ${file_prediction_smoke} ./predicitons/Smoke/${new_prefix}_${no_prefix_name}
+	done
 done
 
