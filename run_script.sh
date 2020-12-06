8a19345151bc4f06952d1f46f5e6c6d41bb128c5
Eduardo-Vilas-Boas <eduardo.vilasboas.guerra@gmail.com>
2020-12-06 15:58:18 +0000

    Corrected script.sh

--- run_script.sh
+++ run_script.sh
@@ -1,44 +1,50 @@
-#/bin/bash
+cd /content/darknet
 
 mkdir ./predictions
 mkdir ./predictions/Neutral
 mkdir ./predictions/Fire
 mkdir ./predictions/Smoke
 
-for file in /content/darknet/data/obj/*.jpg
+for file in ./image/*.jpg
 
 do
+	echo "file: ${file}"
+
 	/content/darknet/darknet detector test data/obj.data cfg/yolov4.cfg ../yolov4_5000.weights ${file} -iou_thresh 0.3 -thresh 0.05
+
+	COUNTER=0
 
 	var="${file%.jpg}"
   	new_prefix="${var##*/}"
 
-	FILE_FIRE=./*Fire.jpg
-	FILE_SMOKE=./*Smoke.jpg	
-
-	if test -f "$FILE_FIRE"; then
-		no_prefix_name="${file##*/}"
-	else
-		if test -f "$FILE_SMOKE"; then
-		else
-			no_prefix_name="${file##*/}"
-			mv ${file} ./predictions/Neutral/${no_prefix_name}
-		fi
-	fi
-
-
-	for file_prediction_fire in ./*Fire.jpg
-	do		
-		#echo "${file_prediction_fire}"
-		no_prefix_name="${file_prediction_fire##*/}"
-		mv ${file_prediction_fire} ./predictions/Fire/${new_prefix}_${no_prefix_name}
-	done
-
-	for file_prediction_smoke in ./*Smoke.jpg
+	for file_prediction_fire in ./*Fire.jpg;
 	do
-		#echo "${file_prediction_smoke}"
-		no_prefix_name="${file_prediction_smoke##*/}"
-		mv ${file_prediction_smoke} ./predictions/Smoke/${new_prefix}_${no_prefix_name}
+		if [[ $file_prediction_fire != "./*Fire.jpg" ]]; then
+			COUNTER=$(( COUNTER + 1 ))
+			echo "Fire: ${file_prediction_fire}"
+			no_prefix_name="${file_prediction_fire##*/}"
+			mv ${file_prediction_fire} ./predictions/Fire/${new_prefix}_${no_prefix_name}
+		fi
 	done
+
+	for file_prediction_smoke in ./*Smoke.jpg;
+	do
+		if [[ $file_prediction_smoke != "./*Smoke.jpg" ]]; then
+			COUNTER=$(( COUNTER + 1 ))
+			echo "Smoke: ${file_prediction_smoke}"
+			no_prefix_name="${file_prediction_smoke##*/}"
+			mv ${file_prediction_smoke} ./predictions/Smoke/${new_prefix}_${no_prefix_name}
+		fi
+	done
+
+
+	echo "Counter: ${COUNTER}"
+
+	if [[ $COUNTER == 0 ]];
+	then
+		no_prefix_name="${file##*/}"
+		echo "${no_prefix_name}"
+		cp ${file} ./predictions/Neutral/${no_prefix_name}
+	fi
 done
 
